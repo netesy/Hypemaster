31a88e9e5173a33c7e064c57c851bb9cea35e7e9
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2023-04-23 13:36:41 +0400

    working version

--- hypemaster.py
+++ hypemaster.py
@@ -1,123 +1,55 @@
-import logging
+from telethon import TelegramClient, events, types
+import os
 import random
-import time
-import requests
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-# Set up logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-
-# Define the ChatGPT API URL
-API_URL = "https://api.openai.com/v1/engines/davinci/completions"
-
-# Define the ChatGPT API parameters
-API_PARAMS = {
-    "prompt": "Tell me a story",
-    "temperature": 0.5,
-    "max_tokens": 1024,
-    "n": 1,
-    "stop": "\n\n",
-}
-
+# Replace the values below with your own API key and phone number
+api_id = 28031946
+api_hash = "245524b69451569529d778b6c21866f0"
+phone_number = "+23054892792"
 # Define the path to the stickers folder
 STICKERS_PATH = "./stickers/"
-
 # Get the list of sticker file names
 STICKER_FILES = os.listdir(STICKERS_PATH)
 
+# Replace the value below with the name of the group you want to post to
+group_link = "https://t.me/takadahHQ"
 
-# Define a command handler
-def start(update: Update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text="Hello, Welcome to the group"
+# Create a new client object and connect to the Telegram API
+client = TelegramClient("my_session", api_id, api_hash)
+
+
+async def main():
+    await client.connect()
+
+    # Log in to the client using your phone number
+    await client.start(phone_number)
+
+    # You can print all the dialogs/conversations that you are part of:
+    # async for dialog in client.iter_dialogs():
+    #     print(dialog.name, "has ID", dialog.id)
+
+    group = await client.get_entity(group_link)
+
+    # You can, of course, use markdown in your messages:
+    message = await client.send_message(
+        "me",
+        "This message has **bold**, `code`, __italics__ and "
+        "a [nice website](https://example.com)!",
+        link_preview=False,
     )
 
+    # You can reply to messages directly if you have a message object
+    await message.reply("Cool!")
 
-# Define a message handler
-def echo(update: Update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-
-# Define a sticker message handler
-def sticker(update: Update, context):
-    if check_activity(update, context):
-        message = update.message
-        # If the user sent a sticker, send a random sticker from the folder
-        sticker_file = random.choice(STICKER_FILES)
-        with open(STICKERS_PATH + sticker_file, "rb") as f:
-            context.bot.send_sticker(chat_id=message.chat_id, sticker=f)
-
-
-def check_activity(update: Update, context) -> bool:
-    # Get a list of all group chats that the bot is a member of
-    chat_list = context.bot.get_chat_members_count()
-
-    # Loop through each group chat and check its activity level
-    for chat in chat_list:
-        if chat["type"] == "group" or chat["type"] == "supergroup":
-            chat_id = chat["chat"]["id"]
-            messages = context.bot.get_chat_history(chat_id=chat_id, limit=1)
-            # Check if the last message was sent more than 5 minutes ago
-            last_activity_time = messages[0].date.timestamp()
-            current_time = time.time()
-            time_diff = current_time - last_activity_time
-
-            if time_diff > 300:
-                return True
-            else:
-                return False
-
-
-# Define a user activity handler
-def activity(update, context):
-    # Check user activity here (e.g. time since last message)
-    if user_is_active:
-        # Make a request to the ChatGPT API and retrieve a random story
-        response = requests.post(
-            API_URL,
-            headers={"Authorization": "6173745258:AAHA7uA2VdBbduMZ0phaAxZoEdFc2OZCTEw"},
-            json=API_PARAMS,
-        )
-        story = response.json()["choices"][0]["text"]
-        context.bot.send_message(chat_id=update.effective_chat.id, text=story)
-    else:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id, text="User is inactive."
+    # Or send files, songs, documents, albums...
+    # Post a sticker to the group
+    sticker_file = random.choice(STICKER_FILES)
+    with open(STICKERS_PATH + sticker_file, "rb") as f:
+        await client.send_file(
+            group,
+            f,
         )
 
 
-# Set up the bot and add handlers
-def main():
-    # Create an Updater object and pass in your bot token
-    # updater = Updater(
-    #     token="6173745258:AAHA7uA2VdBbduMZ0phaAxZoEdFc2OZCTE", use_context=True
-    # )
-
-    updater = Updater("6173745258:AAHA7uA2VdBbduMZ0phaAxZoEdFc2OZCTEw")
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
-
-    # Add command handler
-    dispatcher.add_handler(CommandHandler("start", start))
-
-    # Add message handler
-    dispatcher.add_handler(MessageHandler(Filters.text, echo))
-
-    # Add message handler
-    dispatcher.add_handler(MessageHandler(Filters.all, sticker))
-
-    # Add user activity handler
-    dispatcher.add_handler(MessageHandler(Filters.all, activity))
-
-    # Start the bot
-    updater.start_polling()
-
-    # Run the bot until the user presses Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT
-    updater.idle()
-
-
-if __name__ == "__main__":
-    main()
+with client:
+    client.loop.run_until_complete(main())
