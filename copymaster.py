4957980884bd7b437847c78f7e99e4994ba6bb3e
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2023-04-30 16:04:37 +0400

    update the script to handle multiple users

---
+++ copymaster.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timedelta
+from telethon import TelegramClient, events, types
+import os
+import random
+import asyncio
+
+# Define the list of phone numbers, API keys, and API hashes
+accounts = [
+    {
+        "api_id": 27610862,
+        "api_hash": "2b23114139c8a0deb14505ee681ebb0f",
+        "phone_number": "+2349151221593",
+    },
+    {
+        "api_id": YOUR_API_ID,
+        "api_hash": "YOUR_API_HASH",
+        "phone_number": "+1234567890",
+    },
+    # Add more accounts as needed
+]
+
+# Replace the values below with the links to the source and target groups
+source_group_link = "https://t.me/efatah33metaverse"
+target_group_link = "https://t.me/target_group"
+
+
+async def main(account):
+    # Create a new client object and connect to the Telegram API
+    client = TelegramClient(
+        f"my_session_{account['phone_number']}", account["api_id"], account["api_hash"]
+    )
+    await client.connect()
+
+    # Log in to the client using your phone number
+    await client.start(account["phone_number"])
+
+    source_group = await client.get_entity(source_group_link)
+    target_group = await client.get_entity(target_group_link)
+
+    # Copy members from the source group to the target group
+    async for member in client.iter_participants(source_group):
+        try:
+            await client(InviteToChannelRequest(target_group, [member]))
+        except Exception as e:
+            print(f"Error copying member {member.id} ({member.username}): {e}")
+
+
+# Create a list of tasks for each account
+tasks = [asyncio.create_task(main(account)) for account in accounts]
+
+# Run the tasks
+asyncio.gather(*tasks)
--- hypemaster.py
+++ hypemaster.py
@@ -4,10 +4,21 @@ import os
 import random
 import asyncio
 
-# Replace the values below with your own API key and phone number
-api_id = 27610862
-api_hash = "2b23114139c8a0deb14505ee681ebb0f"
-phone_number = "+2349151221593"
+# Define the list of phone numbers, API keys, and API hashes
+accounts = [
+    {
+        "api_id": 27610862,
+        "api_hash": "2b23114139c8a0deb14505ee681ebb0f",
+        "phone_number": "+2349151221593",
+    },
+    {
+        "api_id": YOUR_API_ID,
+        "api_hash": "YOUR_API_HASH",
+        "phone_number": "+1234567890",
+    },
+    # Add more accounts as needed
+]
+
 # Define the path to the stickers folder
 STICKERS_PATH = "./stickers/"
 # Get the list of sticker file names
@@ -16,23 +27,26 @@ STICKER_FILES = os.listdir(STICKERS_PATH)
 # Replace the value below with the name of the group you want to post to
 group_link = "https://t.me/efatah33metaverse"
 
-# Create a new client object and connect to the Telegram API
-client = TelegramClient("my_session", api_id, api_hash)
 
-
-async def main():
+async def main(account):
+    # Create a new client object and connect to the Telegram API
+    client = TelegramClient(
+        f"my_session_{account['phone_number']}", account["api_id"], account["api_hash"]
+    )
     await client.connect()
 
     # Log in to the client using your phone number
-    await client.start(phone_number)
+    await client.start(account["phone_number"])
 
     group = await client.get_entity(group_link)
 
     # Define the time interval for checking group activity
-    interval = timedelta(minutes=1)
+    minutes = random.randint(1, 9)
+    interval = timedelta(minutes=minutes)
 
     # Define the time threshold for group inactivity
-    threshold = timedelta(minutes=5)
+    minutes = random.randint(10, 15)
+    threshold = timedelta(minutes=minutes)
 
     # Define the last active time as the current time
     last_active_time = datetime.now()
@@ -59,5 +73,8 @@ async def main():
         await asyncio.sleep(interval.total_seconds())
 
 
-with client:
-    client.loop.run_until_complete(main())
+# Create a list of tasks for each account
+tasks = [asyncio.create_task(main(account)) for account in accounts]
+
+# Run the tasks
+asyncio.gather(*tasks)
