20023eea81fb2c7163b5e09fdcbf42fe201e37bb
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2023-05-02 16:22:41 +0400

    Updated the file to use manual translation

--- hypemaster.py
+++ hypemaster.py
@@ -1,8 +1,6 @@
 from datetime import datetime, timedelta
 from telethon import TelegramClient, events, types
-import os
-import random
-import asyncio
+import os, random, asyncio
 from googletrans import Translator
 
 # Define the list of phone numbers, API keys, and API hashes
@@ -25,20 +23,60 @@ STICKERS_PATH = "./stickers/"
 # Get the list of sticker file names
 STICKER_FILES = os.listdir(STICKERS_PATH)
 
-# Define the languages to translate to
-LANGUAGES = ["fr", "es", "pt", "en"]
+MESSAGES = {
+    "en": [
+        "Join EFA network now, grow your portfolio!",
+        "Buy Collact NOW or Regret Later!",
+        "Have you invest wisely?",
+        "Do Your Own Research, EFA Network to the MOON!!",
+        "Anything to share with the group?",
+    ],
+    "pt": [
+        "Junte-se à rede EFA agora, aumente seu portfólio!",
+        "Compre Collact AGORA ou se arrependa depois!",
+        "Você investiu sabiamente?",
+        "Faça sua própria pesquisa, EFA Network to the MOON!!",
+        "Alguma coisa para compartilhar com o grupo?",
+    ],
+    "es": [
+        "Únete a la red EFA ahora, ¡haz crecer tu portafolio!",
+        "¡Compra Collact AHORA o arrepiéntete después!",
+        "¿Has invertido sabiamente?",
+        "¡Haz tu propia investigación, EFA Network to the MOON!!",
+        "¿Algo que compartir con el grupo?",
+    ],
+    "fr": [
+        "Rejoignez le réseau EFA maintenant, développez votre portefeuille !",
+        "Achetez Collact MAINTENANT ou regrettez plus tard !",
+        "Avez-vous investi judicieusement ?",
+        "Faites votre propre recherche, EFA Network to the MOON!!",
+        "Quelque chose à partager avec le groupe ?",
+    ],
+    "de": [
+        "Treten Sie jetzt dem EFA-Netzwerk bei und erweitern Sie Ihr Portfolio!",
+        "Kaufen Sie Collact JETZT oder bereuen Sie es später!",
+        "Haben Sie klug investiert?",
+        "Führen Sie Ihre eigene Recherche durch, EFA Network to the MOON!!",
+        "Etwas mit der Gruppe zu teilen?",
+    ],
+    "nl": [
+        "Sluit je nu aan bij het EFA-netwerk, laat je portefeuille groeien!",
+        "Koop Collact NU of betreur het later!",
+        "Heb je verstandig geïnvesteerd?",
+        "Doe je eigen onderzoek, EFA Network to the MOON!!",
+        "Iets om te delen met de groep?",
+    ],
+    "sv": [
+        "Gå med i EFA-nätverket nu, väx din portfölj!",
+        "Köp Collact NU eller ångra senare!",
+        "Har du investerat klokt?",
+        "Gör din egen forskning, EFA Network to the MOON!!",
+        "Något att dela med gruppen?",
+    ],
+}
 
-# # Define the list of messages to post randomly
-MESSAGES = [
-    "Join EFA network now, grow your portfolio!",
-    "Buy Collact NOW or Regret Later!",
-    "Have you invest wisely?",
-    "Do Your Own Research, EFA Network to the MOON!!",
-    "Anything to share with the group?",
-]
-
-# Create a translator object
-translator = Translator()
+# randomly select one language
+language = random.choice(list(MESSAGES.keys()))
 
 
 # Replace the value below with the name of the group you want to post to
@@ -58,7 +96,7 @@ async def main(account):
     group = await client.get_entity(group_link)
 
     # Define the time interval for checking group activity
-    minutes = random.randint(1, 9)
+    minutes = random.randint(1, 3)
     interval = timedelta(minutes=minutes)
 
     # Define the time threshold for group inactivity
@@ -90,14 +128,9 @@ async def main(account):
                         f,
                     )
             else:
-                # Choose a random message from the English messages list
-                message = random.choice(MESSAGES)
-                # Choose a random language to translate to
-                lang = random.choice(LANGUAGES)
-                # Translate the message to the chosen language
-                translation = translator.translate(message, dest=lang)
-                # Post the translated message to the group
-                await client.send_message(group, translation.text)
+                # randomly select one message from the selected language and print it out
+                message = random.choice(MESSAGES[language])
+                await client.send_message(group, message)
         # Wait for the interval time before checking again
         await asyncio.sleep(interval.total_seconds())
 
